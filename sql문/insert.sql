d56788e68a22eb310cda27ff22cb7cdb371e220d
uommou <ffffff.990917@gmail.com>
2022-11-11 18:00:09 +0900

    Create insert.sql

---
+++ sql문/insert.sql
@@ -0,0 +1,4 @@
+--load data local infile '상대경로' into table team07.테이블명 fields terminated by ',';
+--이 형식 지켜서 csv 파일 삽입하면 됩니다.
+
+load data local infile '상대경로' into table team07.테이블명 fields terminated by ',';
